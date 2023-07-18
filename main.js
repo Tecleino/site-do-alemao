4d441394907a7037c3734707e29100b8e186aa21
Tecleino <98667649+Tecleino@users.noreply.github.com>
2023-07-17 22:15:44 -0300

    commit
    novo botao e musica adicionada

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 function showModal(){
     var element = document.getElementById("modal")
     element.classList.add("show-modal")
+    var audio = document.getElementById("myAudio");
+    audio.volume= 1
+    audio.play();
 }
 
 function hideModal(){
     var element = document.getElementById("modal");
     element.classList.remove("show-modal");
+    var audio = document.getElementById("myAudio");
+    audio.pause();
 }
-
